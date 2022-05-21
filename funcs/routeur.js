7a0bcf9bc5a55e06bef6a3aed8fd579f02de4a08
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-05-21 23:35:26 +0000

    Enregistre l'evenement en base

--- funcs/routeur.js
+++ funcs/routeur.js
@@ -1,7 +1,47 @@
-exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: "Fabdeck" })
+require('dotenv').config();
+
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+
+if (admin.app.length === 0) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.DATABASE_URL
+    });
+}
+
+const db = admin.firestore();
+
+exports.handler = async (e, context) => {
+    let evt = e.queryStringParameters.e
+
+    if (evt) {
+        const err = await updateEvent(evt)
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ erreur: err })
+        }
+    } else {
+        return {
+            statusCode: 401,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ message: "Veuillez renseigner un evenement" })
+        }
+    }
+}
+
+async function updateEvent(evt, cb) {
+    const evtDoc = {
+        targetAt: admin.firestore.FieldValue.serverTimestamp(),
+    }
+
+    try {
+        await db.collection("events").doc(evt).set(evtDoc);
+        return false
+    } catch (err) {
+        console.log(err)
+        return true
     }
 }
